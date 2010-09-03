0333530815e9b47361cc95ce7a5d0aa2d4ef14d3
Myron Marston <myron.marston@gmail.com>
2010-09-03 15:40:06 -0700

    Fixed backtrace so it doesn't include lines from before the autorun at_exit hook.

--- features/step_definitions/additional_cli_steps.rb
+++ features/step_definitions/additional_cli_steps.rb
@@ -9,3 +9,14 @@ Then /^the output should not contain any of these:$/ do |table|
     combined_output.should_not =~ compile_and_escape(string)
   end
 end
+
+Then /^the backtrace\-normalized output should contain:$/ do |partial_output|
+  # ruby 1.9 includes additional stuff in the backtrace,
+  # so we need to normalize it to compare it with our expected output.
+  normalized_output = combined_output.split("\n").map do |line|
+    line =~ /(^\s+# [^:]+:\d+)/ ? $1 : line # http://rubular.com/r/zDD7DdWyzF
+  end.join("\n")
+
+  normalized_output.should =~ compile_and_escape(partial_output)
+end
+
